bbfdf5f26caf5d1dad6074bf0e92f77dc8fe5653
Jah524 <fjshsjf@gmail.com>
2017-04-10 07:35:02 +0900

    fix example

--- src/examples/lstm/simple_prediction.clj
+++ src/examples/lstm/simple_prediction.clj
@@ -1,16 +1,22 @@
 (ns examples.lstm.simple-prediction
   (:require
-    [nn.lstm :as lstm]
+    [sai-ai.nn.lstm :as lstm]
     [clojure.pprint :refer [pprint]]
     [clj-time.local  :as l]))
 
 (defn sum-of-squares-error
   [model training-list]
   (loop [training-list training-list, acc 0]
-    (let [{training-x-seq :x training-y-seq :y} (first training-list)]
+    (let [{training-x-seq :x training-y-seq :y} (first training-list)
+          training-y-seq-keys (map #(if (= :skip %) :skip (keys %)) training-y-seq)]
       (if (and training-x-seq training-y-seq)
-        (let [last-time-output-layer (last (:activation (last (lstm/sequential-output model training-x-seq))))
-              error (->> (mapv #(* 0.5 (- %1 %2) (- %1 %2)) last-time-output-layer (last training-y-seq))
+        (let [error (->> (mapv #(if (= :skip %2)
+                                  0
+                                  (let [it1 (-> %1 :activation :output vals first)
+                                        it2 (-> %2 vals first)]
+                                    (* 0.5 (- it1 it2) (- it1 it2))))
+                               (lstm/sequential-output model training-x-seq training-y-seq-keys)
+                               training-y-seq)
                          (reduce +))]
           (recur (rest training-list) (+ error acc)))
         acc))))
@@ -21,12 +27,13 @@
       (let [{x-seq :x y-seq :y} training-pair
             delta-list (lstm/bptt model
                                   x-seq
-                                  (mapv #(if (= :pass %) :pass (float-array %)) y-seq) lstm-option)]
-        (recur (lstm/update-model model delta-list learning-rate) (rest training-list)))
+                                  y-seq
+                                  lstm-option)]
+        (recur (lstm/update-model! model delta-list learning-rate) (rest training-list)))
       model)))
 
 
-(defn train [model training-list loss-fn lstm-option]
+(defn train-with-demo-dataset [model training-list loss-fn lstm-option]
   (let [{:keys [optimizer learning-rate epoc loss-interval label label-interval]
          :or {optimizer :sgd learning-rate 0.01 epoc 10000 loss-interval 100 label-interval 1000}} lstm-option]
     (loop [model model, e 0]
@@ -42,7 +49,7 @@
           (when (and label (= 0 (rem e label-interval)))
             (println label))
           (when (= 0 (rem e 1000))
-            (println (apply str "[ " (.toUpperCase (name (:model-type model))) " Model ]: "
+            (println (apply str "[ Simple Prediction Model ]: "
                             (->> (map #(:unit-num %) (:layers model))
                                  (cons (count (first (:x (first training-list)))))
                                  (interpose " => ")))))
@@ -53,25 +60,22 @@
 
 (def dataset
   [
-    {:x (map float-array [[1]])         :y (map float-array [[10]])}
-    {:x (map float-array [[1] [1] [1]]) :y (map float-array [[10] [20] [30]])}
-    {:x (map float-array [[1] [2]])     :y (map float-array [[10] [40]])}
-    {:x (map float-array [[1] [1] [1] [1] [1]]) :y [:pass :pass (float-array [30]) :pass (float-array [50])]}
+    {:x (map float-array [[1]])                 :y [{"prediction" 10}]}
+    {:x (map float-array [[1] [1] [1]])         :y [{"prediction" 10} {"prediction" 20} {"prediction" 30}]}
+    {:x (map float-array [[1] [2]])             :y [{"prediction" 10} {"prediction" 40}]}
+    {:x (map float-array [[1] [1] [1] [1] [1]]) :y [:skip :skip {"prediction" 30} :skip {"prediction" 50}]}
     ])
 
 
 (defn demo
-  "Sometimes success with ? lstm units"
+  "captures demo model with 2 lstm units"
   []
-  (let [model (train (lstm/init-lstm-model {:model-type :deep ;or :stack nil (for single hidden layer)
-                                            :layers [{:unit-num 1 :layer-type :input}
-                                                     {:unit-type  :lstm :unit-num 6 :layer-type :hidden}
-                                                     {:unit-num 1 :activate-fn :linear  :layer-type :output}]})
-                     dataset
-                     sum-of-squares-error
-                     {:loss-interval 100
-                      :epoc 2000
-                      :label "LSTM demo"})
+  (let [model (train-with-demo-dataset (lstm/init-model nil #{"prediction"} :dense 1 2 :prediction)
+                                       dataset
+                                       sum-of-squares-error
+                                       {:loss-interval 200
+                                        :epoc 2000
+                                        :label "LSTM demo"})
         demo-input1 (map float-array [[1]])
         demo-input2 (map float-array [[1] [1] [1]])
         demo-input3 (map float-array [[1] [1] [1] [1] [1]])]
@@ -79,15 +83,14 @@
     (pprint dataset)
     (println "\n*** demo1 ***")
     (pprint demo-input1)
-    (pprint (last (:activation (last (lstm/sequential-output model demo-input1)))))
+    (pprint (last (:activation (last (lstm/sequential-output model demo-input1 [#{"prediction"}])))))
     (println "\n*** demo2 ***")
     (pprint demo-input2)
-    (pprint (last (:activation (last (lstm/sequential-output model demo-input2)))))
+    (pprint (last (:activation (last (lstm/sequential-output model demo-input2 [:skip :skip #{"prediction"}])))))
     (println "\n*** demo3 ***")
     (pprint demo-input3)
-    (pprint (last (:activation (last (lstm/sequential-output model demo-input3)))))
+    (pprint (last (:activation (last (lstm/sequential-output model demo-input3 [:skip :skip :skip :skip #{"prediction"}])))))
     (println)))
-
 
 
 (defn -main []
