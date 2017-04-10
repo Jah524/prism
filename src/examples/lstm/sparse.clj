29be1d291f729bc9a592d7bb4b3f300bd3fd8f74
Jah524 <fjshsjf@gmail.com>
2017-04-10 15:42:27 +0900

    Fix sparse example

--- src/examples/lstm/sparse.clj
+++ src/examples/lstm/sparse.clj
@@ -1,16 +1,23 @@
 (ns examples.lstm.sparse
   (:require
     [clojure.pprint :refer [pprint]]
-    [nn.lstm :as lstm]
+    [sai-ai.nn.lstm :as lstm]
     [clj-time.local  :as l]))
+
 
 (defn sum-of-squares-error
   [model training-list]
   (loop [training-list training-list, acc 0]
-    (let [{training-x-seq :x training-y-seq :y} (first training-list)]
+    (let [{training-x-seq :x training-y-seq :y} (first training-list)
+          training-y-seq-keys (map #(if (= :skip %) :skip (keys %)) training-y-seq)]
       (if (and training-x-seq training-y-seq)
-        (let [last-time-output-layer (last (:activation (last (lstm/sequential-output model training-x-seq))))
-              error (->> (mapv #(* 0.5 (- %1 %2) (- %1 %2)) last-time-output-layer (last training-y-seq))
+        (let [error (->> (mapv #(if (= :skip %2)
+                                  0
+                                  (let [it1 (-> %1 :activation :output vals first)
+                                        it2 (-> %2 vals first)]
+                                    (* 0.5 (- it1 it2) (- it1 it2))))
+                               (lstm/sequential-output model training-x-seq training-y-seq-keys)
+                               training-y-seq)
                          (reduce +))]
           (recur (rest training-list) (+ error acc)))
         acc))))
@@ -21,12 +28,12 @@
       (let [{x-seq :x y-seq :y} training-pair
             delta-list (lstm/bptt model
                                   x-seq
-                                  (mapv #(if (= :pass %) :pass (float-array %)) y-seq) lstm-option)]
-        (recur (lstm/update-model model delta-list learning-rate) (rest training-list)))
+                                  y-seq
+                                  lstm-option)]
+        (recur (lstm/update-model! model delta-list learning-rate) (rest training-list)))
       model)))
 
-
-(defn train [model training-list loss-fn lstm-option]
+(defn train-with-demo-dataset [model training-list loss-fn lstm-option]
   (let [{:keys [optimizer learning-rate epoc loss-interval label label-interval]
          :or {optimizer :sgd learning-rate 0.01 epoc 10000 loss-interval 100 label-interval 1000}} lstm-option]
     (loop [model model, e 0]
@@ -42,7 +49,7 @@
           (when (and label (= 0 (rem e label-interval)))
             (println label))
           (when (= 0 (rem e 1000))
-            (println (apply str "[ " (.toUpperCase (name (:model-type model))) " Model ]: "
+            (println (apply str "[ Sparse Model ]: "
                             (->> (map #(:unit-num %) (:layers model))
                                  (cons (count (first (:x (first training-list)))))
                                  (interpose " => ")))))
@@ -53,29 +60,24 @@
 
 (def dataset-sparse
   [
-    {:x [{"A" (float 1)}]                 :y (map float-array [[10]])}
-    {:x [{"A" (float 1)} {"A" (float 1)}] :y (map float-array [[10] [100]])}
-    {:x [{"A" (float 1)} {"B" (float 1)}] :y (map float-array [[10] [20]])}
+    {:x [{"A" (float 1)}]                 :y [{"prediction" 10}]}
+    {:x [{"A" (float 1)} {"A" (float 1)}] :y [{"prediction" 10} {"prediction" 100}]}
+    {:x [{"A" (float 1)} {"B" (float 1)}] :y [{"prediction" 10} {"prediction" 20}]}
     {:x [{"A" (float 1)} {"B" (float 1)} {"C" (float 1)} {"D" (float 1)}]
-     :y [(float-array [10]) (float-array [20]) :pass (float-array [200])]}
+     :y [{"prediction" 10} {"prediction" 20} :skip {"prediction" 200}]}
     {:x [{"A" (float 1)} {"D" (float 1)} {"B" (float 1)} {"C" (float 1)}]
-     :y [:pass (float-array [1]) (float-array [20]) (float-array [-1])]}
+     :y [:skip {"prediction" 1} {"prediction" 20} {"prediction" -1}]}
     ])
 
 (defn sparse-demo
-  "Sometimes success with ? lstm units"
+  "success with 10 lstm units"
   []
-  (let [model (train (lstm/init-lstm-model {:model-type :stack
-                                            :input-type :sparse
-                                            :layers [{:unit-num 1 :layer-type :input}
-                                                     {:unit-type  :lstm :unit-num 10 :layer-type :hidden}
-                                                     {:unit-num 1 :activate-fn :linear  :layer-type :output}]}
-                                           ["A" "B" "C" "D"])
-                     dataset-sparse
-                     sum-of-squares-error
-                     {:loss-interval 100
-                      :epoc 1000
-                      :label "sparse demo"})
+  (let [model (train-with-demo-dataset (lstm/init-model  #{"A" "B" "C" "D"} #{"prediction"} :sparse nil 10 :prediction)
+                                       dataset-sparse
+                                       sum-of-squares-error
+                                       {:loss-interval 100
+                                        :epoc 2000
+                                        :label "sparse demo"})
         demo-input1 [{"A" (float 1)}]
         demo-input2 [{"A" (float 1)} {"A" (float 1)}]
         demo-input3 [{"A" (float 1)} {"B" (float 1)} {"C" (float 1)} {"D" (float 1)}]
@@ -85,19 +87,19 @@
     (pprint dataset-sparse)
     (println "\n*** demo1 ***")
     (println demo-input1)
-    (pprint (last (:activation (last (lstm/sequential-output model demo-input1)))))
+    (pprint (:output (:activation (last (lstm/sequential-output model demo-input1 [#{"prediction"}])))))
     (println "\n*** demo2 ***")
     (println demo-input2)
-    (pprint (last (:activation (last (lstm/sequential-output model demo-input2)))))
+    (pprint (:output (:activation (last (lstm/sequential-output model demo-input2 [:skip #{"prediction"}])))))
     (println "\n*** demo3 ***")
     (println demo-input3)
-    (pprint (last (:activation (last (lstm/sequential-output model demo-input3)))))
+    (pprint (:output (:activation (last (lstm/sequential-output model demo-input3 [:skip :skip :skip #{"prediction"}])))))
     (println "\n*** demo4 ***")
     (println demo-input4)
-    (pprint (last (:activation (last (lstm/sequential-output model demo-input4)))))
+    (pprint (:output (:activation (last (lstm/sequential-output model demo-input4 [:skip :skip #{"prediction"}])))))
     (println "\n*** demo5 ***")
     (println demo-input5)
-    (pprint (last (:activation (last (lstm/sequential-output model demo-input5)))))
+    (pprint (:output (:activation (last (lstm/sequential-output model demo-input5 [:skip :skip :skip #{"prediction"}])))))
     (println)
     ))
 
