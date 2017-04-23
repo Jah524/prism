33d413fc176300eca12d9c61ce7a76a684231865
Jah524 <fjshsjf@gmail.com>
2017-04-24 01:45:34 +0900

    add neanderthal for get native power

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [uncomplicate/neanderthal "0.9.0"]
                  [clj-time "0.13.0"]
                  [com.taoensso/nippy "2.13.0"]
                  [org.clojure/data.json "0.2.6"]
