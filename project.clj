a8b5f8aa990c61d7125830506241d1c3ad9dcc02
Jah524 <fjshsjf@gmail.com>
2017-04-30 16:01:44 +0900

    add dependencies

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :jvm-opts ["-Xmx14G" "-Xms4096m" "-server"]
   :test-selectors {:default (complement :native)
-                   :native :native
+                   :native  :native
                    :all     (constantly true)}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [uncomplicate/neanderthal "0.9.0"]
@@ -13,4 +13,7 @@
                  [com.taoensso/nippy "2.13.0"]
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/core.async "0.3.442"]
-                 [incanter "1.5.7"]])
+                 [incanter "1.5.7"]
+                 [com.googlecode.efficient-java-matrix-library/core "0.26"]
+                 [thinktopic/think.tsne "0.1.1"]
+                 ])
