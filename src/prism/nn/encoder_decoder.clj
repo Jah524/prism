75b775f7a8e5f522c3b1b0f1cc323e3364d97ba9
Jah524 <fjshsjf@gmail.com>
2017-06-02 16:47:31 +0900

    trivial fix

--- src/prism/nn/encoder_decoder.clj
+++ src/prism/nn/encoder_decoder.clj
@@ -5,27 +5,27 @@
   [model x-seq output-items-seq]
   (let [{:keys [rnn-type]} model]
     (condp = rnn-type
-      :lstm (lstm/forward model x-seq output-items-seq)
+      :lstm (lstm/encoder-decoder-forward model x-seq output-items-seq)
       :gru :fixme)))
 
 (defn bptt
   [model activation output-items-seq]
   (let [{:keys [rnn-type]} model]
     (condp = rnn-type
-      :lstm (lstm/bptt model activation output-items-seq)
+      :lstm (lstm/encoder-decoder-bptt model activation output-items-seq)
       :gru :fixme)))
 
 (defn update-model!
   [model param-delta-list learning-rate]
   (let [{:keys [rnn-type]} model]
     (condp = rnn-type
-      :lstm (lstm/update-model! model param-delta-list learning-rate)
+      :lstm (lstm/update-encoder-decoder! model param-delta-list learning-rate)
       :gru :fixme)))
 
 (defn init-model
   [{:keys [rnn-type] :as params}]
   (condp = rnn-type
-    :lstm (lstm/init-model params)
+    :lstm (lstm/init-encoder-decoder-model params)
     :gru :fixme
     (throw (Exception. "rnn-type was not specified"))))
 
